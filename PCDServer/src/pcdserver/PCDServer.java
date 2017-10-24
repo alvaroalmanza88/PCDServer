1db596bc739f9463e0c5adf0d93b2c4a0041a895
alvaroalmanza88 <alvaro.almanza88@gmail.com>
2017-10-24 18:47:08 +0200

    Construccion Base Proyecto
    Se crea Proyecto en Netbeans para empezar a trabajar de manera colaborativa con el grupo.

---
+++ PCDServer/src/pcdserver/PCDServer.java
@@ -0,0 +1,20 @@
+/*
+ * CODIGO REALIZADO POR ALVARO ALMANZA.
+ * UNIVERSIDAD INTERNACIONAL I DE CASTILLA
+ */
+package pcdserver;
+
+/**
+ *
+ * @author alvar
+ */
+public class PCDServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
